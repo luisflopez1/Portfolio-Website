7c0840200ecf98d868933a6ce04b11b6c9ab986a
luis lopez <Luis.lopez0417@gmail.com>
2024-07-16 11:18:31 -0400

    added javascript
    added a simple function in javascript

---
+++ resources/scripts/script.js
@@ -0,0 +1,15 @@
+
+let testFunction = (number) =>{
+
+	let newNumber = number + 2;
+
+	return newNumber;
+
+}
+
+let profile = document.getElementById("profile");
+
+profile.addEventListener("click", function() {
+  profile.src = "../resources/images/contact.jpg";
+
+ });
